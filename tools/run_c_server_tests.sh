d21969578efdd7119cbed0fce0e433c701cb1ae2
Zkh-dot <kernzahar@gmail.com>
2026-07-05 00:18:21 +0300

    feat: parse battlesnake json in native server

--- tools/run_c_server_tests.sh
+++ tools/run_c_server_tests.sh
@@ -9,3 +9,14 @@ mkdir -p build/tests
   -o build/tests/test_arena
 
 build/tests/test_arena
+
+"${CC:-cc}" -std=c2x -D_POSIX_C_SOURCE=200809L -Ibattlesnake/c-core \
+  tests/c/test_battlesnake_json.c \
+  battlesnake/c-core/server/arena.c \
+  battlesnake/c-core/server/battlesnake_json.c \
+  battlesnake/c-core/datatypes/coord.c \
+  battlesnake/c-core/datatypes/snake.c \
+  battlesnake/c-core/datatypes/board.c \
+  -o build/tests/test_battlesnake_json
+
+build/tests/test_battlesnake_json
